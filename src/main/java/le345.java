96e2804fa274cd0ec30979122001b5540e1acd13
moyoungjun <mo1212123@naver.com>
2022-11-04 09:50:57 +0900

    first commit

---
+++ src/main/java/le345.java
@@ -0,0 +1,20 @@
+
+public class le345 {
+    public static void main(String args[]){
+        String test = "hello";
+        char a = 'a';
+        char e = 'e';
+        char i = 'i';
+        char o = 'o';
+        char u = 'u';
+        int leng = test.length();
+        String resut = "";
+        System.out.println(test.charAt(0));
+        for(int f = 0; f<leng ; f++){
+            if(test.charAt(i) == e){
+
+            }
+        }
+
+    }
+}
