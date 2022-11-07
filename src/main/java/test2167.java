95a043c2a05ff494f3805ff6652cee85e1a9a5db
mo1212123 <mo1212123@gmail.com>
2022-11-07 23:32:47 +0900

    Feat: 백준 2차원배열

--- src/main/java/test2167.java
+++ src/main/java/test2167.java
@@ -5,21 +5,26 @@ public class test2167 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        System.out.println(n);
         int[][] arr = new int[n][m];
-        for(int i =0 ; i<arr.length; i++){
-            for(int j =0; j<arr[i].length; j++){
+        for(int i =0 ; i<n; i++){
+            for(int j =0; j<m; j++){
                arr[i][j] = sc.nextInt();
             }
         }
         int s = sc.nextInt();
-        for(int i =0 ; i < s; i++){
+        while(s-- >0){
+            int sum = 0;
             int o = sc.nextInt();
             int p = sc.nextInt();
             int k = sc.nextInt();
             int w = sc.nextInt();
-            int i1 = arr[o][p];
-            int i2 = arr[k][w];
+            for(int q = o-1 ; q<k; q++){
+                for(int j=p-1;j<w; j++){
+                    sum = arr[q][j]+sum;
+                }
+
+            }
+            System.out.println(sum);
 
 
         }
