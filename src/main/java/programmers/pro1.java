79f698daabfa3e8f12adc0b72fe284a7fbb80a9b
mo1212123 <mo1212123@gmail.com>
2022-11-27 23:25:02 +0900

    Feat: 프로그래머스 1 단계 (명예의 전당)

---
+++ src/main/java/programmers/pro1.java
@@ -0,0 +1,32 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class pro1 {
+    public static void main(String args[]){
+        int [] score = {10,100,20,150,1,100,200};
+        int a =3;
+        int b=7;
+        int min ;
+        int minIndex;
+        int [] array = new int[a];
+        ArrayList test = new ArrayList();
+        for(int i =0; i < a; i++){
+            array[i] = score[i];
+        }
+        for(int i = 0 ; i < array.length; i++){
+
+        }
+        Arrays.stream(array).sorted();
+
+
+        System.out.println( test );
+//        System.out.println( test.indexOf(test.stream().min()) );
+        for(int i = a ; i < score.length;i++){
+            if()
+        }
+
+    }
+}
