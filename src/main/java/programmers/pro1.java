0fc3b3665b413c1ba06fe3a62b21f6709ef0653d
mo1212123 <mo1212123@gmail.com>
2022-11-29 00:25:16 +0900

    Feat: 프로그래머스 1 단계 (명예의 전당)

--- src/main/java/programmers/pro1.java
+++ src/main/java/programmers/pro1.java
@@ -5,28 +5,36 @@ import java.util.Arrays;
 
 
 public class pro1 {
-    public static void main(String args[]){
+    public static void main(String[] args){
         int [] score = {10,100,20,150,1,100,200};
         int a =3;
         int b=7;
         int min ;
-        int minIndex;
-        int [] array = new int[a];
+        int minIndex=0;
+
+        int [] answer = new int[a];
         ArrayList test = new ArrayList();
-        for(int i =0; i < a; i++){
-            array[i] = score[i];
-        }
-        for(int i = 0 ; i < array.length; i++){
+        for(int i=0; i< score.length; i++){
+            if(i<a)
+            {
+                answer[i] = score[i];
+                test.add(answer[0]);
+
+            }
+            else {
+                if(answer[0] < score[i]){
+                    answer[0] = score[i];
+                    Arrays.sort(answer);
+                }
+                test.add(answer[0]);
+
+
+            }
+
 
         }
-        Arrays.stream(array).sorted();
 
 
-        System.out.println( test );
-//        System.out.println( test.indexOf(test.stream().min()) );
-        for(int i = a ; i < score.length;i++){
-            if()
-        }
-
+        System.out.println(test);
     }
 }
