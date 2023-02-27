b40ca48deec6ef394d33d22b515eccb57a19eea9
moyoungjun <mo1212123@naver.com>
2023-02-27 14:11:37 +0900

    Feat: DP (le121번)문제 초기 커밋

---
+++ src/main/java/leetcode/le121_BestTimetoBuy.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.OptionalInt;
+
+public class le121_BestTimetoBuy {
+    public static void main(String args[]) {
+        int[] prices = {7,1,5,3,6,4};
+
+        int profit = 0;
+    }
+}
