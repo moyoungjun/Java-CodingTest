c066405f70dc81bb105f16336294eef4e7f8d5b9
moyoungjun <mo1212123@naver.com>
2023-03-07 10:40:12 +0900

    Fix: stack 20번

--- src/main/java/leetcode/le20_ValidPaerntheses.java
+++ src/main/java/leetcode/le20_ValidPaerntheses.java
@@ -4,15 +4,32 @@ import java.util.Stack;
 
 public class le20_ValidPaerntheses {
     public static void main(String args[]) {
-        String s = "(){}";
-        Stack<String> stack = new Stack<>();
-        stack.push(s);
-        System.out.println(stack.get(0));
-//        for(int i= s.length()-1; i > 0; i--) {
-//            if(s.substring(i - 1, i).equals(stack.elementAt(i))){
-//                stack.pop();
-//            }
-//        }
-        System.out.println(stack);
+        String s = "{}()";
+        int len = s.length();
+        boolean ans = true;
+        if(len % 2 != 0) {ans= false;}
+
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < len; i++) {
+            char curn = s.charAt(i);
+
+            if(curn == '(' || curn == '{' || curn == '[') {
+                stack.push(curn);
+            }
+
+            else if(stack.size() == 0) {ans= false;}
+
+            else if(curn == ')') {
+                if(stack.pop() != '(') {ans= false;}
+            }
+            else if(curn == '}') {
+                if(stack.pop() != '{') {ans= false;}
+            }
+            else if(curn == ']') {
+                if(stack.pop() != '[') {ans= false;}
+            }
+        }
+        ans= stack.size() == 0;
     }
 }
