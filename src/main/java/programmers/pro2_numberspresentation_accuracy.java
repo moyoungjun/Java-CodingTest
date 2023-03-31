18bd6c5197ab5aebd53b0c4a999daf88965caa45
moyoungjun <mo1212123@naver.com>
2023-03-31 11:34:06 +0900

    Fix: 2중 for문,조건 break 사용- 효율성 통과

--- src/main/java/programmers/pro2_numberspresentation_accuracy.java
+++ src/main/java/programmers/pro2_numberspresentation_accuracy.java
@@ -3,19 +3,16 @@ package programmers;
 public class pro2_numberspresentation_accuracy {
     public static void main(String args[]) {
         int n = 15;
-        int[] sum = new int[n];
         int count = 1;
-        int left = 0;
-        for(int i= 1; i<n; i ++){
-            sum[i] = sum[i-1] + i;
-            if(sum[i] == n){
-                count ++;
-            }
-        }
-        for(int i=1; i<n;i++){
-            for(int j=i+1; j<n-1;j++) {
-                if(sum[j]-sum[i] == n) {
+        for(int i=1; i<=n;i++){
+            int sum = i;
+            for(int j=i+1; j<=n;j++) {
+                sum = sum + j;
+                if(sum == n) {
                     count++;
+                    break;
+                } else if (sum>n) {
+                    break;
                 }
             }
         }
